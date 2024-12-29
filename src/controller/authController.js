24f601cc335991160d70fb621d8b15b1a95a69aa
kevin.dev <KevinBell.dev@gmail.com>
2024-12-29 13:14:37 -0300

    prueba del dia 5

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -57,7 +57,7 @@ export const registerController = async (req, res) => {
         return res.json(response)
     }
 
-    /* const hashedPassword = await bcrypt.hash(registerConfig.password.value, 10)
+    const hashedPassword = await bcrypt.hash(registerConfig.password.value, 10)
     const validation_token = jsonwebtoken.sign({
         email: registerConfig.email.value
     },
@@ -66,13 +66,16 @@ export const registerController = async (req, res) => {
         expiresIn: '1d'
     }
     
-    ) */
+    )
+
+    const redirectUrl = `${ENVIROMENT.URL_FRONTEND}/api/auth/verify-email/` + validation_token
 
     const result = await transporterEmail.sendMail({
         subject: 'Valida tu email',
         to: registerConfig.email.value,
         html: `
         <h1>Valida tu mail</h1>
+        <p>Para validar tu mail haz click en <a href= "${redirectUrl}">Este enlace</a></p>
         `
     })
 
@@ -113,4 +116,17 @@ export const registerController = async (req, res) => {
     }
 }
 
-
+export const verifyEmailController = async (req, res) =>{
+    try{
+        const {validation_token} = req.params
+        const payload = jsonwebtoken.verify(validation_token, ENVIROMENT.SECRET_KEY) 
+        const email_to_verify = payload.email
+        const user_to_verify = await User.findOne({email: email_to_verify})
+        user_to_verify.emailVerified = true
+        await user_to_verify.save()
+        res.json({message: 'Email verificado'})
+        /* redirect, al front end */
+    }catch(error){
+        console.error(error)
+    }
+}
